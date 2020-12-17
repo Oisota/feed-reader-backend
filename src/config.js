d1f5aeeb46561558a22edbd9b92044dae82d16cd
Derek Morey <derek.o.morey@gmail.com>
2020-12-16 23:34:26 -0500

    Updated config, added supervisor config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 exports.PORT = process.env.PORT || 8081;
 exports.COOKIE_SECRET = process.env.COOKIE_SECRET || 'go go power rangers';
+exports.ENV = process.env.NODE_ENV;
 exports.IN_PROD = process.env.NODE_ENV === 'production';
-exports.DB_FILE = './data/app.db';
+exports.DB_FILE = process.env.DB_FILE || './data/app.db';
